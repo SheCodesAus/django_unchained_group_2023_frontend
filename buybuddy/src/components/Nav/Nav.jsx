a9d4130921a1b6ca5251866332d38595bdd5f993
niku153 <104482078+niku153@users.noreply.github.com>
2023-04-01 12:30:32 +0800

    Nav and Homepage pages updated

--- buybuddy/src/components/Nav/Nav.jsx
+++ buybuddy/src/components/Nav/Nav.jsx
@@ -1,14 +1,59 @@
-//Hooks
-
-//Pages
-
-//Components
-
-//CSS
+import { Link, useNavigate } from "react-router-dom";
+// import logo from "../../media/buybuddy-logo.png";
 import "./Nav.css";
-
-function Nav() {
-  return <div>This is the Nav</div>;
+import { useRef, useState } from "react";
+// Nav Bar
+function Nav(props) {
+  const [navbar, setNavbar] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hiddend");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  // added a state variable showMenu that toggles the visibility of the links when the menu button is clicked
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  const navRef = useRef();
+  const navigate = useNavigate();
+  const { loggedIn, setLoggedIn } = props;
+  const handleClick = () => {
+    window.localStorage.removeItem("token");
+    setLoggedIn(false);
+    navigate("/");
+    navRef.current.classList.toggle("responsive_nav");
+  };
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+  const changeNavbar = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbar);
+  return (
+    <div className="nav-wrapper">
+      <div className="navbar">
+        <div className="logo-wrapper">
+          {/* <img src={logo} alt="Logo" className="logo-img" /> */}
+        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {/* to do: menu icon toggles into an 'x and for auth-buttons to go into hamburger menu when minimised' */}
+          {showMenu ? "Close" : "Menu"}
+        </button>
+        <div className={`links ${showMenu ? "show" : ""}`}>
+          <a href="#howitworks">How it works</a>
+          <a href="#faq">FAQ</a>
+          <a href="#">Team</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className={`auth-buttons ${showMenu ? "show" : ""}`}>
+          <button className="login-btn">Login</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default Nav;
--- buybuddy/src/pages/Homepage/Homepage.jsx
+++ buybuddy/src/pages/Homepage/Homepage.jsx
@@ -1,18 +1,40 @@
-//Hooks
+import { useState, useEffect } from "react";
 
-//Pages
+import "./HomePage.css";
 
-//Components
-
-//CSS
-import "./Homepage.css";
-
-function Homepage() {
+function HomePage() {
   return (
-    <div>
-      <h1>This is the Homepage</h1>
+    // hero banner
+    <div className="hero-container">
+      <div className="hero-content-wrapper">
+        {/* app title + description */}
+        <div className="hero-section-1">
+          <div className="hero-content">
+            <h1 className="hero-title">Purchasing decisions made easy.</h1>
+            <p className="hero-description">
+              Save your time, money while you still indulging with retail
+              therapy.
+            </p>
+            <button className="cta-button">Take Control</button>
+          </div>
+        </div>
+      </div>
+      {/* photo */}
+      <div className="hero-image-wrapper">
+        <div className="hero-section-2">
+          <div className="hero-image-content">
+            <div className="hero-image"></div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    // second section - moving css testiomonials
+    // third section - image 1 - usp/features
+    // fourth section - image 2 - usp/features
+    // fifth section - image 3 - usp/features
+    // sixth - cta
   );
 }
 
-export default Homepage;
+export default HomePage;
