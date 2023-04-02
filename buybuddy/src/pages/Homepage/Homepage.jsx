9ec26540adcfec79f80a257b2fe09dac119b081b
Adrianna <chongadrianna@gmail.com>
2023-04-02 22:37:11 +0800

    homepage styling

--- buybuddy/src/pages/Homepage/Homepage.jsx
+++ buybuddy/src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import heroimg from "../../assets/hero comp.png";
 import "./HomePage.css";
 
 function HomePage() {
@@ -8,17 +8,16 @@ function HomePage() {
     <div className="hero__container">
             <div class="split">
                 <div class="hero-left hidden">
-                    <h1 class="h1">The new way to shop.</h1>
+                    <h1 class="h1">The New Way To Shop.</h1>
                     <p class="body">Too many choices, too little time? Say goodbye to shopping anxiety with BestBuddy - your new shopping bestie. </p> 
                     <div class="btn-wrap">
                         {/* <a href="#" class="btn subheading">sign up</a> */}
                     </div>
                 </div>
                 <div class="hero-right hidden">
-                    {/* <img src="images/hero comp.png" alt=""> */}
-                </div>
+                    <img src={heroimg} className="hero-img" alt="hero" />
             </div>
-        
+        </div>
     </div>
 
 );
