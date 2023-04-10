e9af2e043bae3f16d6a4278e8bb052a50dd9b636
Adrianna <chongadrianna@gmail.com>
2023-04-10 22:49:59 +0800

    team page

---
+++ buybuddy/src/pages/MeetTheTeam/MeetTheTeam.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+function MeetTheTeam() {
+    return (
+        <div className="team-container-left">
+            <div className="team-name">Team member 1</div>
+            <div className="team-descirption">Team member 2</div>
+            <div className="team-link">linked in</div>
+            <div className="team-picture"></div>
+
+        </div>
+
+
+
+    );
+};
+
+export default MeetTheTeam;
--- buybuddy/src/pages/NotFound/NotFound.jsx
+++ buybuddy/src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function NotFound() {
+function NotFound() {
     return (
-        <div>
-            <h1>Oops! You seem to be lost.</h1>
-            <p>Here are some helpful links:</p>
-            <Link to='/'>Home</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to='/contact'>Contact</Link>
+        <div className="404-container">
+            <div className="404-descirption">
+                <h1>Oops! You seem to be lost.</h1>
+                <p>Here are some helpful links:</p>
+                <Link to='/'>Home</Link>
+                <Link to="/how-it-works">How It Works</Link>
+                <Link to="/contact-us">Contact</Link>
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default NotFound;
