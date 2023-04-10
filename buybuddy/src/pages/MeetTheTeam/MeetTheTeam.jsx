fb0cb99a041e81a57d4037b86079c4bb8368b768
Adrianna <chongadrianna@gmail.com>
2023-04-10 22:53:04 +0800

    meet the team

--- buybuddy/src/pages/MeetTheTeam/MeetTheTeam.jsx
+++ buybuddy/src/pages/MeetTheTeam/MeetTheTeam.jsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from "react";
 
 function MeetTheTeam() {
     return (
-        <div className="team-container-left">
-            <div className="team-name">Team member 1</div>
-            <div className="team-descirption">Team member 2</div>
-            <div className="team-link">linked in</div>
-            <div className="team-picture"></div>
+        <div className="team-main-container'>
+            <div className="team-container-left">
+                <div className="team-name">Team member 1</div>
+                <div className="team-descirption">Team member 2</div>
+                <div className="team-link">linked in/github/email</div>
+                <div className="team-picture"></div>
+            </div>
 
+            <div className="team-container-right">
+                <div className="team-name">Team member 1</div>
+                <div className="team-descirption">Team member 2</div>
+                <div className="team-link">linked in/github/email</div>
+                <div className="team-picture"></div>
+            </div>
         </div>
-
 
 
     );
